6ba754b4a1ce44dc8283f5851db04e14bb986597
OZX-OG <53302924+OZX-OG@users.noreply.github.com>
2021-05-24 01:18:57 +0200

    Update Voice Recorder.py

--- Voice Recorder.py
+++ Voice Recorder.py
@@ -1,3 +1,4 @@
+#import libraries
 import pyaudio
 import wave
 import colorama
@@ -5,43 +6,50 @@ from keyboard import is_pressed
 from time import sleep
 from colorama import Fore
 
+#Color
 colorama.init(autoreset=True)
 
+#You Know ;)
 print(f"{Fore.YELLOW}--{Fore.GREEN} Voice Recorder By: OZX-OG {Fore.YELLOW} --")
 
+#var
 STOP = "s"
 i = 0
 
+#settings
 audio_rec = pyaudio.PyAudio()
 
 stream = audio_rec.open(channels=1, rate=44100, format=pyaudio.paInt16, input=True, frames_per_buffer=1024)
 
 frames = []
 
-
+#print info
 print(f"You can stop recording by Pressing: {STOP}")
 input("press Enter To start recording: ")
 
+#clear cmd
 if name == 'nt': system('cls')
 else: system("clear") 
 
 print(f"-{Fore.RED} s {Fore.WHITE}- to stop")
 print(f"{Fore.YELLOW}-{Fore.GREEN} iS Recording... {Fore.YELLOW}-")
 
-
+#rec
 while True:
     data = stream.read(1024)
     frames.append(data)
     i += 1
 
     if is_pressed(STOP):
+        #end rec
         stream.stop_stream()
         stream.close()
         audio_rec.terminate()
         print(f"{Fore.RED}- End Record -")
         break
 
-    
+
+#settings audio file
 file_name = input("Enter Audio-File name: ")
 sound_file = wave.open(f"{file_name}.wav", "wb")
 
